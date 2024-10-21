96f241eeab018169eda3cfb5ed96e895f4b8fc92
unknown <you@example.com>
2024-10-21 12:30:00 +0200

    Added the api/artists endpoint

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('/api/artists', (req, res) => {
+    res.send([
+        "JB",
+        "Beyonce",
+    ]);
+});
+
 // Starten van de server en op welke port de server moet luisteren
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
